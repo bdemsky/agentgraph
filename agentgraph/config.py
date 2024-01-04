f25eed7e33d71d4f7f8a8fe66a2adb2429089146
Brian Demsky <bdemsky@uci.edu>
2024-01-04 14:47:15 -0800

    Add support for memoization for debugging

--- agentgraph/config.py
+++ agentgraph/config.py
@@ -1,2 +1,6 @@
-# Maximum number of outstanding tasks per scheduler
 MAX_WINDOW_SIZE = 64
+# Maximum number of outstanding tasks per scheduler
+
+
+DEBUG_PATH = "./debug"
+# Debug path.  None if debugging is not enabled.
--- agentgraph/core/llmmodel.py
+++ agentgraph/core/llmmodel.py
@@ -1,4 +1,8 @@
 import asyncio
+import hashlib
+import agentgraph.config
+import json
+from pathlib import Path
 from openai import AsyncAzureOpenAI
 
 class LLMModel:
@@ -11,7 +15,59 @@ class LLMModel:
         self.switchThreshold = threshold
         self.largeModel = largeModel
         
+    async def lookupCache(self, message_to_send) -> str:
+        if agentgraph.config is None:
+            return None
+        encoded = json.dumps(message_to_send)
+        hash = hashMessage(encoded)
+        first = hash[0:2]
+        second = hash[2:4]
+        path = Path(agentgraph.config.DEBUG_PATH).absolute()
+        full_path = path / first / second
+        matches = full_path.glob(hash+"*.entry")
+        for key in matches:
+            contents = key.read_text()
+            if contents == encoded:
+                val_path = Path(str(key) + ".val")
+                if val_path.exists():
+                    contents = val_path.read_text()
+                    return contents
+        return None
+    
+    def writeCache(self, message_to_send, response):
+        if agentgraph.config is None:
+            return
+        encoded = json.dumps(message_to_send)
+        hash = hashMessage(encoded)
+        first = hash[0:2]
+        second = hash[2:4]
+        path = Path(agentgraph.config.DEBUG_PATH).absolute()
+        full_path = path / first / second
+        full_path.mkdir(parents=True, exist_ok=True)
+        count = 1
+        while True:
+            name = hash + "-" + str(count) + ".entry"
+            path = full_path / name
+            if not path.exists():
+                break
+            contents = key.read_text()
+            if contents == encoded:
+                break
+            count = count + 1
+            
+        val_path = Path(str(path) + ".val")
+        tmpkey = full_path / "tmp.key"
+        tmpval = full_path / "tmp.val"
+        tmpkey.write_text(encoded)
+        tmpval.write_text(response)
+        tmpval.rename(val_path)
+        tmpkey.rename(path)
+                
     async def sendData(self, message_to_send) -> str:
+        cache_result = await self.lookupCache(message_to_send)
+        if cache_result != None:
+            return cache_result
+
         totallen = 0
         for msg in message_to_send:
             totallen += len(msg["content"])
@@ -33,4 +89,13 @@ class LLMModel:
                 print("Retrying due to failure from openai\n")
 
         chat_message = chat_completion.choices[0].message.content
+        self.writeCache(message_to_send, chat_message)
         return chat_message
+
+def hashMessage(message_to_send: str) -> str:
+    """ Returns hash of message_to_send"""
+    
+    hasher = hashlib.sha1()
+    hasher.update(message_to_send.encode())
+    return hasher.digest().hex()
+    
