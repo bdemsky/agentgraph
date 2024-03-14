d81837fee28da5c7d62771d33c9d48a65e819d5c
Conan Truong <cjtruong@uci.edu>
2024-03-13 17:23:37 -0700

    Added stalling example

---
+++ examples/stalling/example.py
@@ -0,0 +1,57 @@
+import agentgraph
+import os
+import time
+from agentgraph.exec.engine import Engine
+
+
+def testFuncA1(scheduler, i, start) -> list:
+    time.sleep(0.5)
+    print("TestA1", i, time.time() - start)
+    out_var = scheduler.runPythonAgent(testFuncA2, pos=[i + 1, start], numOuts=1)
+    out_var = scheduler.runPythonAgent(testFuncA3, pos=[out_var, start], numOuts=1)
+    
+    out_val = out_var.getValue()
+    print("TestA4", out_val, time.time() - start)
+    return []
+
+def testFuncA2(scheduler, i, start) -> list:
+    time.sleep(0.5)
+    print("TestA2", i, time.time() - start)
+    return [i + 1]
+
+def testFuncA3(scheduler, i, start) -> list:
+    time.sleep(0.5)
+    print("TestA3", i, time.time() - start)
+    return [i + 1]
+
+
+def testFuncB1(scheduler, i, start) -> list:
+    time.sleep(0.5)
+    print("TestB1", i, time.time() - start)
+    out_var = scheduler.runPythonAgent(testFuncB2, pos=[i + 1, start], numOuts=1)
+    out_var2 = scheduler.runPythonAgent(testFuncB3, pos=[i + 2, start], numOuts=1)
+    
+    out_val = out_var.getValue()
+    out_val2 = out_var2.getValue()
+    print("TestB4", out_val, out_val2, time.time() - start)
+    return []
+
+def testFuncB2(scheduler, i, start) -> list:
+    time.sleep(0.5)
+    print("TestB2", i, time.time() - start)
+    return [i + 1]
+
+def testFuncB3(scheduler, i, start) -> list:
+    time.sleep(1)
+    print("TestB3", i, time.time() - start)
+    return [i + 1]
+
+model = agentgraph.LLMModel("https://demskygroupgpt4.openai.azure.com/", os.getenv("OPENAI_API_KEY"), "GPT4-8k", "GPT-32K", 34000)
+NUM_THREADS = 1
+scheduler = agentgraph.getRootScheduler(model, Engine(concurrency=NUM_THREADS))
+start = time.time()
+out_var = None
+for i in range(NUM_THREADS):
+    scheduler.runPythonAgent(testFuncA1, pos=[i * 10 + 10, start])
+    scheduler.runPythonAgent(testFuncB1, pos=[i * 10 + 10, start])
+scheduler.shutdown()
